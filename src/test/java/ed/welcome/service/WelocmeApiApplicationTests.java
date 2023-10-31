19b8388b6184e93e10c3bf57fa78ac962f1aa054
saubhagya <patrasaubhagya146@gmail.com>
2023-10-31 20:41:46 +0530

    Added another API i.e welcome API

---
+++ src/test/java/ed/welcome/service/WelocmeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ed.welcome.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelocmeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
